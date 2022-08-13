29519f544a14ff45b03d1d9f368e303a025b727e
Nelson <gmz_nlsn@yahoo.com>
2022-08-13 14:08:24 -0700

    work completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const saturdayFun = (arg = 'roller-skate') => {
+    return `This Saturday, I want to ${arg}!`;
+}
+
+const mondayWork = (activity = 'go to the office') => {
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = (string = '*') => (param1 = 'special')=> `You are ${string}${param1}${string}!`;
+
+// console.log(wrapAdjective());
+
+// const wrapAdjective = (string = '*') => {
+//     return function name(param1 = 'special') {
+       
+//         return `You are ${string}${param1}${string}!`
+//     }
+// }
